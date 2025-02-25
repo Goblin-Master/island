02b37f6882122ecbc6a29e49ed1a68a99c32739a
Goblin-master <2896151192@qq.com>
2025-02-25 21:53:56 +0800

    feat: 为ai增加上下文功能

--- utils/cacheUtils/enter.go
+++ utils/cacheUtils/enter.go
@@ -7,6 +7,13 @@ import (
 	"time"
 )
 
+// SaveContent
+//
+//	@Description: 用于ai对话保存上下文
+//	@param ctx
+//	@param key
+//	@param history
+//	@param reply
 func SaveContent(ctx context.Context, key string, history, reply string) {
 	if len(history)+len(reply) > 4000 {
 		err := global.Rdb.Set(ctx, key, reply, time.Second*3600).Err()
@@ -19,6 +26,14 @@ func SaveContent(ctx context.Context, key string, history, reply string) {
 		zlog.Errorf("redis存储错误:%v", err)
 	}
 }
+
+// GetContent
+//
+//	@Description: 获取ai对话的上下文
+//	@param ctx
+//	@param key
+//	@return string
+//	@return error
 func GetContent(ctx context.Context, key string) (string, error) {
 	value, err := global.Rdb.Get(ctx, key).Result()
 	if err != nil {
