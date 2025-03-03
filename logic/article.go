b7111899d2f0aa6579fc49c668c8b728a00dbba6
Goblin-master <2896151192@qq.com>
2025-03-03 20:48:06 +0800

    fix: 修改下点赞参数

--- logic/article.go
+++ logic/article.go
@@ -79,6 +79,8 @@ func (l *ArticleLogic) ArticleDelete(ctx context.Context, req list.RemoveReq) (r
 
 func (l *ArticleLogic) ArticleDigg(ctx context.Context, req types.ArticleDiggReq) (resp string, err error) {
 	defer utils.RecordTime(time.Now())()
+	// TODO:获取用户id填进去
+	req.UserID = int64(793478004095)
 	db := repo.NewArticleRepo(global.DB)
 	resp, err = db.ArticleDigg(ctx, req)
 	return
