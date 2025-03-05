99ce0a041113878d7c61438d6e703819261bf0f8
Goblin-master <2896151192@qq.com>
2025-03-05 18:27:22 +0800

    fix: 解决岛屿名字删除后冲突，直接去掉软删除

--- model/island.go
+++ model/island.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
@@ -9,14 +8,13 @@ type Island struct {
 	ID        int64 `gorm:"primaryKey;column:id;type:bigint;uniqueIndex:idx_name"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-	Name      string         `gorm:"column:name;type:varchar(64);not null;unique;comment:'名称'"`
-	Path      string         `gorm:"column:path;type:varchar(255);not null;comment:'岛屿图片'"`
-	Width     float64        `gorm:"column:width;type:float;not null;comment:'宽度'"`
-	Height    float64        `gorm:"column:height;type:float;not null;comment:'高度'"`
-	XPoint    float64        `gorm:"column:xPoint;type:float;not null;comment:'x坐标'"`
-	YPoint    float64        `gorm:"column:yPoint;type:float;not null;comment:'y坐标'"`
-	UserID    int64          `gorm:"column:user_id;type:bigint;not null;comment:'用户id';uniqueIndex:idx_name"`
+	Name      string  `gorm:"column:name;type:varchar(64);not null;unique;comment:'名称'"`
+	Path      string  `gorm:"column:path;type:varchar(255);not null;comment:'岛屿图片'"`
+	Width     float64 `gorm:"column:width;type:float;not null;comment:'宽度'"`
+	Height    float64 `gorm:"column:height;type:float;not null;comment:'高度'"`
+	XPoint    float64 `gorm:"column:xPoint;type:float;not null;comment:'x坐标'"`
+	YPoint    float64 `gorm:"column:yPoint;type:float;not null;comment:'y坐标'"`
+	UserID    int64   `gorm:"column:user_id;type:bigint;not null;comment:'用户id';uniqueIndex:idx_name"`
 }
 
 func (i *Island) TableName() string {
