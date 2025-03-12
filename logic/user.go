6ef67562cc271bcd7b7460b261a0f3eac6ea283c
Goblin-master <2896151192@qq.com>
2025-03-12 15:00:52 +0800

    feat: 增加关注列表查看功能

--- logic/user.go
+++ logic/user.go
@@ -5,7 +5,9 @@ import (
 	"strconv"
 	"tgwp/global"
 	"tgwp/log/zlog"
+	"tgwp/model"
 	"tgwp/repo"
+	"tgwp/repo/list"
 	"tgwp/response"
 	"tgwp/types"
 	"tgwp/utils"
@@ -54,4 +56,43 @@ func (l *UserLogic) FocusUser(ctx context.Context, req types.FocusUserReq) (resp
 		zlog.CtxErrorf(ctx, "用户关注相关操作失败:%v", err)
 	}
 	return
+}
+
+func (l *UserLogic) FocusList(ctx context.Context, req types.FocusListReq) (resp types.FocusListResp, err error) {
+	defer utils.RecordTime(time.Now())()
+	user_id, err := strconv.ParseInt(req.UserID, 10, 64)
+	if err != nil {
+		zlog.CtxErrorf(ctx, "类型转换:%v", err)
+		return types.FocusListResp{}, response.ErrResp(err, response.COMMON_FAIL)
+	}
+	focusIDList, err := repo.NewUserRepo(global.DB).FocusIDList(user_id)
+	if err != nil {
+		zlog.CtxErrorf(ctx, "获取用户关注列表失败:%v", err)
+		return types.FocusListResp{}, response.ErrResp(err, response.COMMON_FAIL)
+	}
+	if len(focusIDList) == 0 {
+		return types.FocusListResp{}, nil
+	}
+	_list, count, err := list.ListQuery(model.User{}, list.Options{
+		PageInfo: req.PageInfo,
+		Order:    "created_at desc",
+		Where:    global.DB.Where("id in ?", focusIDList),
+	})
+	if err != nil {
+		zlog.CtxErrorf(ctx, "获取用户关注列表失败:%v", err)
+		return types.FocusListResp{}, response.ErrResp(err, response.COMMON_FAIL)
+	}
+	var focusList []types.FocusList
+	for _, v := range _list {
+		focusList = append(focusList, types.FocusList{
+			Avatar:    v.Avatar,
+			CreatedAt: v.CreatedAt,
+			UserID:    v.ID,
+			Username:  v.Username,
+		})
+	}
+	return types.FocusListResp{
+		Count: count,
+		List:  focusList,
+	}, nil
 }
--- types/user.go
+++ types/user.go
@@ -1,14 +1,33 @@
 package types
 
+import (
+	"tgwp/repo/list"
+	"time"
+)
+
 type UserDetailReq struct {
 	UserID string `form:"user_id"`
 }
 type UserDetailResp struct {
-	Username  string `json:"username"`
-	Avatar    string `json:"avatar"`
-	CreatedAt string `json:"created_at"`
+	Username  string    `json:"username"`
+	Avatar    string    `json:"avatar"`
+	CreatedAt time.Time `json:"created_at"`
 }
 type FocusUserReq struct {
 	UserID  int64  `form:"user_id"`
 	FocusID string `form:"focus_id"`
+}
+type FocusListReq struct {
+	list.PageInfo
+	UserID string `form:"user_id"`
+}
+type FocusList struct {
+	UserID    int64     `json:"user_id,string"`
+	Username  string    `json:"username"`
+	Avatar    string    `json:"avatar"`
+	CreatedAt time.Time `json:"created_at"`
+}
+type FocusListResp struct {
+	Count int         `json:"count"`
+	List  []FocusList `json:"list"`
 }
